00d9a69cd3eb1254aedb739d73b19fcb72ad60eb
Rajsekhar Das <rajsekhardas2003@gmail.com>
2024-07-01 21:09:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def start():
     playAssistantSound()
 
 
-    os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+    os.system('start msedge.exe --app="http://localhost:8000/indexx.html"')
 
-    eel.start('index.html', mode=None, host='localhost', block=True)
+    eel.start('indexx.html', mode=None, host='localhost', block=True)
