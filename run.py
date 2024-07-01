50f10c1b4b06e33029a1ac2e9fb40a12562ec8fc
Rajsekhar Das <rajsekhardas2003@gmail.com>
2024-07-01 22:23:34 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,35 +1,32 @@
- 
-
-import multiprocessing
+import threading
 import subprocess
 
-# To run Jarvis
 def startJarvis():
-        # Code for process 1
-        print("Process 1 is running.")
-        from main import start
-        start()
+    # Code for process 1
+    print("Process 1 is running.")
+    from main import start
+    start()
 
-# To run hotword
 def listenHotword():
-        # Code for process 2
-        print("Process 2 is running.")
-        from engine.features import hotword
-        hotword()
+    # Code for process 2
+    print("Process 2 is running.")
+    from engine.features import hotword
+    hotword()
 
-
-    # Start both processes
 if __name__ == '__main__':
-        multiprocessing.set_start_method('spawn')
-        p1 = multiprocessing.Process(target=startJarvis)
-        p2 = multiprocessing.Process(target=listenHotword)
-        p1.start()
-        subprocess.call([r'device.bat'])
-        p2.start()
-        p1.join()
+    thread1 = threading.Thread(target=startJarvis)
+    thread2 = threading.Thread(target=listenHotword)
+    
+    thread1.start()
+    subprocess.call([r'device.bat'])
+    thread2.start()
+    
+    thread1.join()
+    
+    if thread2.is_alive():
+        # There's no direct way to terminate a thread in Python,
+        # you would need to use a flag or some other method to signal the thread to stop.
+        print("Thread 2 is still running. Implement a stop mechanism if needed.")
+        thread2.join()
 
-        if p2.is_alive():
-            p2.terminate()
-            p2.join()
-
-        print("system stop")
+    print("system stop")
