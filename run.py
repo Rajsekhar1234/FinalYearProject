6125f5510245bdf2bb775d1418fb8fb1cd5afe45
Rajsekhar Das <rajsekhardas2003@gmail.com>
2024-07-01 23:00:20 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,24 +1,34 @@
+ 
+
+import multiprocessing
 import subprocess
-from concurrent.futures import ThreadPoolExecutor
 
+# To run Jarvis
 def startJarvis():
-    print("Process 1 is running.")
-    from main import start
-    start()
+        # Code for process 1
+        print("Process 1 is running.")
+        from main import start
+        start()
 
+# To run hotword
 def listenHotword():
-    print("Process 2 is running.")
-    from engine.features import hotword
-    hotword()
+        # Code for process 2
+        print("Process 2 is running.")
+        from engine.features import hotword
+        hotword()
 
+
+    # Start both processes
 if __name__ == '__main__':
-    with ThreadPoolExecutor(max_workers=2) as executor:
-        future1 = executor.submit(startJarvis)
+        p1 = multiprocessing.Process(target=startJarvis)
+        p2 = multiprocessing.Process(target=listenHotword)
+        p1.start()
         subprocess.call([r'device.bat'])
-        future2 = executor.submit(listenHotword)
-        
-        # Wait for the tasks to complete
-        future1.result()
-        future2.result()
+        p2.start()
+        p1.join()
 
-    print("system stop")
+        if p2.is_alive():
+            p2.terminate()
+            p2.join()
+
+        print("system stop")
